d90697f25b2e8b1cf4e350b826e0fd6be7c74004
balachander1205 <digumarthi.bala@gmail.com>
2024-05-05 22:25:39 +0530

    Added nav bar profile

--- src/main/java/com/api/university/controller/appointment/AppointmentController.java
+++ src/main/java/com/api/university/controller/appointment/AppointmentController.java
@@ -3,19 +3,24 @@ package com.api.university.controller.appointment;
 import com.api.university.entity.AppointmentsEntity;
 import com.api.university.model.AppointmentModel;
 import com.api.university.repository.AppointmentsRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpSession;
 import java.sql.Timestamp;
 import java.util.List;
 
 @Controller
 @RequestMapping("/api")
+@Slf4j
 public class AppointmentController {
 
     @Autowired
@@ -38,5 +43,13 @@ public class AppointmentController {
     public ResponseEntity getAppointmentsByRepname(@RequestParam("repname") String repname){
         List<AppointmentsEntity> listOfAppointments = appointmentsRepository.getAppointmentsByRepname(repname);
         return ResponseEntity.ok(listOfAppointments);
+    }
+
+    @RequestMapping("/appointmentDetails")
+    public String appointmentDetails(HttpSession httpSession, Model model, RedirectAttributes redir) {
+        log.info("appointmentDetails:userId={}",httpSession.getAttribute("userId"));
+        String userId = httpSession.getAttribute("userId").toString();
+        model.addAttribute("userId",userId);
+        return "appointments";
     }
 }
